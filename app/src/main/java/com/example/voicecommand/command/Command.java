54623b2ddcd14ef4f7b8ad61517c897e0395fe6b
francescobarioni <francescobarioni18@gmail.com>
2023-03-03 14:12:55 +0100

    fix

--- app/src/main/java/com/example/voicecommand/command/Command.java
+++ app/src/main/java/com/example/voicecommand/command/Command.java
@@ -4,14 +4,15 @@ import android.speech.tts.TextToSpeech;
 
 import androidx.annotation.NonNull;
 
+import com.example.voicecommand.utility.TextToSpeechManager;
+
 import java.util.ArrayList;
 
 public class Command {
 
     public void repeatCommand(@NonNull TextToSpeech textToSpeech) {
         // usa il text-to-speech per far ripetere l'istruzione all'utente
-        textToSpeech.speak("Mi dispiace, non ho capito o il comando non è presente! Potresti per favore ripetere?",
-                TextToSpeech.QUEUE_FLUSH, null, "messageId");
+        TextToSpeechManager.speak("Mi dispiace, non ho capito o il comando non è presente! Potresti per favore ripetere?");
     }
 
     public boolean isCommandPresent(String stringa, @NonNull ArrayList<String> arrayList){
